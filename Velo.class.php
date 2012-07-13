393b4974e7aa7f70a7c96e2256206d7feb9eb36c
Maarten Cautreels <maarten@flatturtle.com>
2012-07-13 14:52:00 +0200

    Fixed Velo REST Service

--- Velo.class.php
+++ Velo.class.php
@@ -7,15 +7,21 @@
  * @author Pieter Colpaert <pieter aŧ iRail.be>
  */
 
- 
 class BikesVelo extends AReader{
 
     public function __construct($package, $resource, $RESTparameters) {
         parent::__construct($package, $resource, $RESTparameters);	
+        $this->lat = null;
+        $this->long = null;
+        $this->offset = 0;
+        $this->rowcount = 1024;
     }
 
     public static function getParameters(){
-        return array();
+        return array(   "lat" => "Latitude" 
+                        ,"long" => "Longitude"
+                        ,"offset" => "Offset"
+                        ,"rowcount" => "Rowcount");
     }
 
     public static function getRequiredParameters(){
@@ -23,11 +29,54 @@ class BikesVelo extends AReader{
     }
 
     public function setParameter($key,$val){
+        if($key == "lat") {
+            $this->lat = $val;
+        } else if ($key == "long") {
+            $this->long = $val;
+        } else if ($key == "offset") {
+            $this->offset = $val;
+        } else if ($key == "rowcount") {
+            $this->rowcount = $val;
+        }
     }
 
     public function read(){
-	
-        return "A resource for bikes in a certain area";
+        $data = TDT::HttpRequest("http://ClearChannelBikes:WbRpWHk6Ur@m.velo-antwerpen.be/data/stations.json");
+        $decoded = json_decode($data->data);
+        //todo: convert to wished format
+        
+        $result = array();
+        $gpoint = new gPoint();
+        
+        foreach($decoded as $sourceStation) {
+            $station = new Object();
+            
+            $station->name = $sourceStation->name;
+            $station->freebikes = $sourceStation->bikes;
+            $station->freespots = $sourceStation->slots;
+            $station->state = $sourceStation->status;
+            $station->latitude = $sourceStation->latitude;
+            $station->longitude = $sourceStation->longitude;
+            
+            $gpoint->setLongLat($station->longitude, $station->latitude);
+            
+            if($this->lat != null && $this->long != null) {
+                $station->distance = $gpoint->distanceFrom($this->long, $this->lat);
+            }
+            
+            array_push($result, $station);
+        }
+        
+        function compare($a, $b) {
+            if ($a->distance == $b->distance) {
+                return 0;
+            }
+            return ($a->distance < $b->distance) ? -1 : 1;
+        }
+        
+        usort($result, "compare");
+        
+        return array_slice($result, $this->offset, $this->rowcount);
     }
 
     public static function getDoc(){
